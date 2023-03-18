691b16244752ccd20bfb749f1483edf72a310da1
hidek1 <hidekazu5200@gmail.com>
2023-03-18 12:45:53 +0900

    first commit

---
+++ app/Http/Controllers/ChatGptController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChatGptController extends Controller
+{
+
+    /**
+     * index
+     *
+     * @param  Request  $request
+     */
+    public function index(Request $request)
+    {
+        return view('chat');
+    }
+
+    /**
+     * chat
+     *
+     * @param  Request  $request
+     */
+    public function chat(Request $request)
+    {
+        // バリデーション
+        $request->validate([
+            'sentence' => 'required',
+        ]);
+
+        // 文章
+        $sentence = $request->input('sentence');
+
+        // TODO: ChatGPT API処理
+
+        return view('chat', compact('sentence'));
+    }
+}
